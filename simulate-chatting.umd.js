f2ecea53260fee3474e03b24d4f5b39a55e29789
cycjimmy <cycjimmy@gmail.com>
2024-07-17 07:14:01 +0000

    Deploying to gh-pages from @ cycjimmy/simulate-chatting@c8c6d7d2f92b6add6d0c453bf43e881f222e3914 🚀

--- simulate-chatting.umd.js
+++ simulate-chatting.umd.js
@@ -4,41 +4,33 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.SimulateChat = factory());
 })(this, (function () { 'use strict';
 
+  function _classCallCheck(a, n) {
+    if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function");
+  }
+  function _defineProperties(e, r) {
+    for (var t = 0; t < r.length; t++) {
+      var o = r[t];
+      o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o);
+    }
+  }
+  function _createClass(e, r, t) {
+    return r && _defineProperties(e.prototype, r), Object.defineProperty(e, "prototype", {
+      writable: !1
+    }), e;
+  }
   function _toPrimitive(t, r) {
     if ("object" != typeof t || !t) return t;
     var e = t[Symbol.toPrimitive];
     if (void 0 !== e) {
-      var i = e.call(t, r || "default");
+      var i = e.call(t, r );
       if ("object" != typeof i) return i;
       throw new TypeError("@@toPrimitive must return a primitive value.");
     }
-    return ("string" === r ? String : Number)(t);
+    return (String )(t);
   }
   function _toPropertyKey(t) {
     var i = _toPrimitive(t, "string");
     return "symbol" == typeof i ? i : i + "";
-  }
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-  function _defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor);
-    }
-  }
-  function _createClass(Constructor, protoProps, staticProps) {
-    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) _defineProperties(Constructor, staticProps);
-    Object.defineProperty(Constructor, "prototype", {
-      writable: false
-    });
-    return Constructor;
   }
 
   /**
@@ -57,7 +49,7 @@
   function styleInject(css, ref) {
     if (ref === void 0) ref = {};
     var insertAt = ref.insertAt;
-    if (!css || typeof document === 'undefined') {
+    if (typeof document === 'undefined') {
       return;
     }
     var head = document.head || document.getElementsByTagName('head')[0];
@@ -80,7 +72,7 @@
   }
 
   var css_248z = ".style__chartList,.style__container,.style__fullImg,.style__swiperWrapper{position:absolute;z-index:1}.style__chartList .style__listContent,.style__footer{position:relative;z-index:1}.style__container,.style__fullImg,.style__swiperWrapper{height:100%;left:0;top:0;width:100%}.style__footer,.style__main{height:0;width:100%}.style__container{background-color:#ebebeb;display:flex;flex-direction:column;overflow:hidden}.style__main{flex:1;overflow:hidden}.style__chartList{display:flex;flex-direction:column;left:0;list-style:none;top:0;width:100%}.style__chartList.swiper-slide{height:auto}.style__chartList>li{display:none;margin:1.8% 0;width:100%}.style__chartList>li:first-of-type{margin-top:3.6%}.style__chartList>li.style__show{display:flex}.style__chartList .style__listContent{margin:0 2.5%}.style__chartList .style__center{justify-content:center}.style__chartList .style__left{justify-content:flex-start}.style__chartList .style__right{justify-content:flex-end}.style__footer{margin-top:1.8%}";
-  var style = {"fullImg":"style__fullImg","container":"style__container","swiperWrapper":"style__swiperWrapper","chartList":"style__chartList","listContent":"style__listContent","footer":"style__footer","main":"style__main","show":"style__show","center":"style__center","left":"style__left","right":"style__right"};
+  var style = {"chartList":"style__chartList","swiperWrapper":"style__swiperWrapper","container":"style__container","fullImg":"style__fullImg","footer":"style__footer","listContent":"style__listContent","main":"style__main","show":"style__show","center":"style__center","left":"style__left","right":"style__right"};
   styleInject(css_248z);
 
   /**
